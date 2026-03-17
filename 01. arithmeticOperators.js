7e3594fd90b90dd0ac162ba3aa127a1a7a20498b
giridharank2727 <giridharank2727@gmail.com>
2026-03-17 13:43:13 +0530

    placement

--- 01. arithmeticOperators.js
+++ 01. arithmeticOperators.js
@@ -5,10 +5,16 @@ let b = 20;
 let c;
 
 c = a + b;     // +  → Addition operator       // Output: 120
+console.log(c);
 c = a - b;     // -  → Subtraction operator    // Output: 80
+console.log(c);
 c = a * b;     // *  → Multiplication operator // Output: 2000
+console.log(c);
 c = a / b;     // /  → Division operator       // Output: 5
+console.log(c);
 c = a % b;     // %  → Modulus operator        // Output: 0
+console.log(c);
 c = 2 ** 3;    // ** → Exponentiation operator // Output: 8
 
+console.log(c);
 
