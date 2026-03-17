aacb5cb927dae6bd0ab48e59f2ea89bf684906e1
giridharank2727 <giridharank2727@gmail.com>
2026-03-17 12:08:06 +0530

    placement

---
+++ index.js
@@ -0,0 +1,4 @@
+let n1=7;
+let n2="7";
+console.log(n1 == n2);
+console.log(n1 === n2);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+function varExample() {
+  var count = 10;
+
+
+  var count = 20;
+
+
+  count = 30;
+
+  console.log("var count:", count);
+}
+varExample();
